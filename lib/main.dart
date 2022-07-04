2f468d500f2b3480b4b533896f9a4e72ed0d5f33
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-07-04 16:55:54 -0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -17,36 +18,43 @@ void main() {
 class HomePage extends StatefulWidget {
   @override
   _HomePage createState() => _HomePage();
-
 }
 
 class _HomePage extends State<HomePage> {
-  String _url1 = 'https://static3.depositphotos.com/1001920/140/i/450/depositphotos_1400122-stock-photo-beach.jpg';
-  List _url2 = ['https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png , https://blog.cobasi.com.br/wp-content/uploads/2020/07/boston-terrier-capa1.png'];
+  String _url1 =
+      'https://static3.depositphotos.com/1001920/140/i/450/depositphotos_1400122-stock-photo-beach.jpg';
+  List _url2 = [
+    'https://cdn.pixabay.com/photo/2022/06/19/04/25/cat-7271017_960_720.jpg'
+  ];
+  void alteracaoImagem() {
+    int numeroAleatorio = Random().nextInt(_url2.length);
+    setState(() {
+      _url1 = _url2[numeroAleatorio];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Minha barra'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Image.network('$_url1'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.black,
-                  textStyle: const TextStyle(fontSize: 40)),
-              onPressed: () {
-                print('Fui clicado');
-              },
-              child: const Text('Clique aqui'),
-            )
-          ],
+        appBar: AppBar(
+          title: const Text('Minha barra'),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Image.network('$_url1'),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                      textStyle: const TextStyle(fontSize: 40)),
+                  onPressed: alteracaoImagem,
+                  child: const Text('Clique aqui'),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
